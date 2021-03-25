2b814e159b66fe40350c89b324d608c512652f06
neyoa <itstomsmart@gmail.com>
2021-03-25 20:25:39 +0000

    Added example.js

---
+++ other/example.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const chalk = require('chalk');
+
+module.exports = {
+	name: 'example',
+	aliases: ['ex', 'dummy'],
+	usage: 'copy and paste me!',
+	description: 'example command',
+	execute(message, args) {
+		// Stuff to do
+	},
+};
